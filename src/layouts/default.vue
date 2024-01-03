957e7de78e0d5fe32fd02f3b6bf0fdc740861793
Paloma Tejeda <palomatejeda81@gmail.com>
2024-01-03 19:25:13 +0100

    menu update for tablets

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -6,6 +6,22 @@ useJsonld(() => ({
   alternateName: "INSEEC Chambéry",
   url: "https://chamberycampus.com/",
   logo: "https://chamberycampus.com/_nuxt/logo-inseec.a0368931.svg",
+  description:
+    "L'INSEEC Chambéry est une école de commerce et de management située en Savoie. Elle propose des formations de Bac à Bac+5 en alternance et en initial.",
+  address: {
+    "@type": "PostalAddress",
+    streetAddress: "12 Avennue du Lac d'Annecy",
+    addressLocality: "Le Bourget-du-Lac",
+    addressRegion: "Auvergne Rhône Alpes",
+    postalCode: "73381",
+    addressCountry: "France",
+  },
+  geo: {
+    "@type": "GeoCoordinates",
+    latitude: "45.64395498437948",
+    longitude: "5.870843396323475",
+  },
+  hasMap: "https://maps.app.goo.gl/JBZPMwA5SvdKDTok9",
   sameAs: [
     "https://www.facebook.com/INSEEC.Chambery/",
     "https://www.instagram.com/inseec_campus_chambery/",
