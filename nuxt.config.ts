000a63e8fd3f08e81ef15adfc4ea97c95c909f11
Paloma Tejeda <palomatejeda81@gmail.com>
2024-01-09 14:54:48 +0100

    performance test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,3 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-
 import { getNews } from "./src/utils/sitemap.js";
 
 export default defineNuxtConfig({
@@ -47,24 +45,6 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
-      // script: [
-      //   {
-      //     hid: "gtmHead",
-      //     innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      // new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      // j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      // 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      //       })(window, document, 'script', 'dataLayer', 'GTM-W8N9CXHL');`,
-
-      //     defer: true,
-      //     async: true,
-      //   },
-      //   {
-      //     src: "https://www.googletagmanager.com/gtag/js?id=AW-355120123",
-      //     innerHTML: `window.dataLayer = window.dataLayer || []; function gtag(){ dataLayer.push(arguments); } gtag('js', new Date()); gtag('config', 'AW-355120123')`,
-      //     async: true,
-      //   },
-      // ],
       htmlAttrs: { lang: "fr" },
     },
   },
