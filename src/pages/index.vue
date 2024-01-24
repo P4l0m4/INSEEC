c1dbbbcdbf6de682fa381fd91f49aa83d8bdfcdb
Paloma Tejeda <palomatejeda81@gmail.com>
2024-01-24 16:39:22 +0100

    changed images on index page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -698,7 +698,7 @@ pc"
         align-items: flex-end;
         padding: 1rem;
         width: 100%;
-        height: 200px;
+        height: 300px;
         color: $primary-color;
         // text-align: center;
         background-image: linear-gradient(
