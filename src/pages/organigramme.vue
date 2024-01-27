9c9c1620cacd59f334e203d5a971683e486ecc2a
Paloma Tejeda <palomatejeda81@gmail.com>
2024-01-27 17:39:22 +0100

    banner length

--- src/pages/organigramme.vue
+++ src/pages/organigramme.vue
@@ -193,7 +193,7 @@ useHead(() => {
     background-image: url("@/assets/images/placeholder.svg");
     background-size: cover;
     background-position: center;
-    height: calc(100svh - 88px);
+    height: calc(100svh - 84px);
     max-height: 1200px;
     width: 100%;
     padding: 1rem;
@@ -207,7 +207,7 @@ useHead(() => {
       align-items: flex-start;
       padding: 2rem;
       gap: 2rem;
-      height: calc(100svh - 80px);
+      height: calc(100svh - 76px);
     }
 
     &::after {
